c0d224f958b9e8ffa17bcbe93f8419a6cf50d3b9
XwyzWorm <pratamaazmi1@gmail.com>
2021-03-27 12:28:44 +0700

    add biodata implementation

--- app/src/main/java/com/pritim/myapplication/ui/Biodata/BiodataViewModel.kt
+++ app/src/main/java/com/pritim/myapplication/ui/Biodata/BiodataViewModel.kt
@@ -1,4 +1,13 @@
 package com.pritim.myapplication.ui.Biodata
 
-class BiodataViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BiodataViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Biodata Fragment"
+    }
+    val text: LiveData<String> = _text
 }
