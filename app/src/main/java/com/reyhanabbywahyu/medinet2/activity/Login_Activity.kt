14fd8be509766f95514332bd261dc2d497d8124e
XwyzWorm <pratamaazmi1@gmail.com>
2021-03-28 10:10:20 +0700

    Add implementation For login

--- app/src/main/java/com/reyhanabbywahyu/medinet2/activity/Login_Activity.kt
+++ app/src/main/java/com/reyhanabbywahyu/medinet2/activity/Login_Activity.kt
@@ -1,12 +1,47 @@
 package com.reyhanabbywahyu.medinet2.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.reyhanabbywahyu.medinet2.R
+import com.reyhanabbywahyu.medinet2.`class`.User
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login_Activity : AppCompatActivity() {
+    lateinit  var user : User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        Log.d("tikuk","78yiuhj")
+        btnMasuk.setOnClickListener {
+            Log.d("tikuk","jkuh")
+            Toast.makeText(this,"TOLOL",Toast.LENGTH_LONG).show()
+            var etMasukEmailtext: String = etMasukEmail.text.toString()
+            var etMasukPassword: String = etMasukPassword.text.toString()
+            user = User(etMasukEmailtext, etMasukPassword, "Roy")
+
+            if (validate_login(user)) {
+                Toast.makeText(this,"Login Berhasi",Toast.LENGTH_LONG).show()
+                loginSuccesfull()
+            }
+            else {
+                Toast.makeText(this, " Login gagal TOLOL",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    fun validate_login(User : User) : Boolean {
+
+        if(User.email == "kacang" && User.password == "123456") {
+            return true
+        }
+        return false
+    }
+
+    fun loginSuccesfull() {
+        intent = Intent(this,UtamaActivity::class.java)
+        intent.putExtra("EXTRA_USER",user)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/reyhanabbywahyu/medinet2/class/User.kt
+++ app/src/main/java/com/reyhanabbywahyu/medinet2/class/User.kt
@@ -1,4 +1,7 @@
 package com.reyhanabbywahyu.medinet2.`class`
 
-class User {
-}
+import androidx.versionedparcelable.ParcelField
+import java.io.Serializable
+
+data class User (
+        val email: String , var password : String , var nama_user : String ) : Serializable
